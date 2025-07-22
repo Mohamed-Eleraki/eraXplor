b4dfabb1fd415334713a5a051075bf355e928a6c
eraki <mohamed-ibrahim2021@outlook.com>
2025-07-22 20:27:52 +0300

    update cost export function

--- src/eraXplor/utils/cost_export_utils.py
+++ src/eraXplor/utils/cost_export_utils.py
@@ -7,6 +7,7 @@ import boto3
 from rich.live import Live
 from rich.spinner import Spinner
 
+
 class CostRecord(TypedDict):
     """Class type annotation tool dettermining the List Schema.
     Type definition for a single cost record.
@@ -15,7 +16,7 @@ class CostRecord(TypedDict):
     time_period: Dict[str, str]  # {'Start': str, 'End': str}
     account_id: str
     account_cost: str
-    
+
 
 def monthly_account_cost_export(
     start_date_input: Union[str, datetime],  # str | datetime
@@ -27,20 +28,20 @@ def monthly_account_cost_export(
     """Retrieves AWS account cost data for a specified time period using AWS Cost Explorer.
 
     Fetches the unblended costs for all linked accounts, services, purchase type, or usage type
-    in an AWS organization for a given date range, grouped by account ID and returned in 
+    in an AWS organization for a given date range, grouped by account ID and returned in
     monthly granularity.
 
     Args:
         - start_date_input (str): The start date of the cost report in YYYY-MM-DD format. Default: Six months ago.
-        
+
         - end_date_input (str): The end date of the cost report in YYYY-MM-DD format. Default: Today.
-        
+
         - aws_profile_name_input (str): The name of the AWS profile to use for authentication,
             as configured in the local AWS credentials file. Default: 'default'.
-            
-        - cost_groupby_key_input (str): The key to group costs by (`LINKED_ACCOUNT`, `SERVICE`, 
+
+        - cost_groupby_key_input (str): The key to group costs by (`LINKED_ACCOUNT`, `SERVICE`,
             `PURCHASE_TYPE`, `USAGE_TYPE`). Default: `LINKED_ACCOUNT`.
-        
+
         - granularity (str): The granularity of the cost data, either 'MONTHLY' or 'DAILY'. Default: 'MONTHLY'.
 
 
@@ -49,7 +50,7 @@ def monthly_account_cost_export(
             - TIME_PERIOD (dict): Contains 'Start' and 'End' dates for the time period.
             - ID (str): The AWS account, service, purchase type, or usage type.
             - COST (str): The unblended cost amount as a string.
-            
+
     """
 
     profile_session = boto3.Session(profile_name=str(aws_profile_name_input))
@@ -57,31 +58,119 @@ def monthly_account_cost_export(
 
     # if condition determine the type of groupby key
     results = []
-    with Live(Spinner
-              ("bouncingBar", text=f"Fetching AWS costs grouped by {cost_groupby_key_input}...\n\n"),
-                refresh_per_second=10):
+    with Live(
+        Spinner(
+            "bouncingBar",
+            text=f"Fetching AWS costs grouped by {cost_groupby_key_input}...\n\n",
+        ),
+        refresh_per_second=10,
+    ):
+
         def fetch_account():
-            account_cost_usage = ce_client.get_cost_and_usage(
-                TimePeriod={"Start": str(start_date_input), "End": str(end_date_input)},
-                # Granularity="MONTHLY",
-                Granularity=granularity,
-                Metrics=["UnblendedCost"],
-                GroupBy=[  # group the result based on account ID
-                    {"Type": "DIMENSION", "Key": cost_groupby_key_input}
-                ],
-            )
-            for item in account_cost_usage["ResultsByTime"]:
-                time_period = item["TimePeriod"]
-                for group in item["Groups"]:
-                    ID = group["Keys"][0]
-                    cost = group["Metrics"]["UnblendedCost"]["Amount"]
-                    results.append(
-                        {
-                            "TIME_PERIOD": time_period,
-                            "ID": ID,
-                            "COST": cost,
-                        }
-                    )
+            if cost_groupby_key_input == "LINKED_ACCOUNT-With-SERVICE":
+                account_cost_usage = ce_client.get_cost_and_usage(
+                    TimePeriod={
+                        "Start": str(start_date_input),
+                        "End": str(end_date_input),
+                    },
+                    # Granularity="MONTHLY",
+                    Granularity=granularity,
+                    Metrics=["UnblendedCost"],
+                    GroupBy=[
+                        {"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"},
+                        {"Type": "DIMENSION", "Key": "SERVICE"},
+                    ],
+                )
+                for item in account_cost_usage["ResultsByTime"]:
+                    time_period = item["TimePeriod"]
+                    for group in item["Groups"]:
+                        ID = group["Keys"][0]
+                        cost = group["Metrics"]["UnblendedCost"]["Amount"]
+                        results.append(
+                            {
+                                "TIME_PERIOD": time_period,
+                                "ID": ID,
+                                "COST": cost,
+                            }
+                        )
+            if cost_groupby_key_input == "LINKED_ACCOUNT-With-PURCHASE_TYPE":
+                account_cost_usage = ce_client.get_cost_and_usage(
+                    TimePeriod={
+                        "Start": str(start_date_input),
+                        "End": str(end_date_input),
+                    },
+                    # Granularity="MONTHLY",
+                    Granularity=granularity,
+                    Metrics=["UnblendedCost"],
+                    GroupBy=[
+                        {"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"},
+                        {"Type": "DIMENSION", "Key": "PURCHASE_TYPE"},
+                    ],
+                )
+                for item in account_cost_usage["ResultsByTime"]:
+                    time_period = item["TimePeriod"]
+                    for group in item["Groups"]:
+                        ID = group["Keys"][0]
+                        cost = group["Metrics"]["UnblendedCost"]["Amount"]
+                        results.append(
+                            {
+                                "TIME_PERIOD": time_period,
+                                "ID": ID,
+                                "COST": cost,
+                            }
+                        )
+            if cost_groupby_key_input == "LINKED_ACCOUNT-With-USAGE_TYPE":
+                account_cost_usage = ce_client.get_cost_and_usage(
+                    TimePeriod={
+                        "Start": str(start_date_input),
+                        "End": str(end_date_input),
+                    },
+                    # Granularity="MONTHLY",
+                    Granularity=granularity,
+                    Metrics=["UnblendedCost"],
+                    GroupBy=[
+                        {"Type": "DIMENSION", "Key": "LINKED_ACCOUNT"},
+                        {"Type": "DIMENSION", "Key": "USAGE_TYPE"},
+                    ],
+                )
+                for item in account_cost_usage["ResultsByTime"]:
+                    time_period = item["TimePeriod"]
+                    for group in item["Groups"]:
+                        ID = group["Keys"][0]
+                        cost = group["Metrics"]["UnblendedCost"]["Amount"]
+                        results.append(
+                            {
+                                "TIME_PERIOD": time_period,
+                                "ID": ID,
+                                "COST": cost,
+                            }
+                        )
+            else:
+                account_cost_usage = ce_client.get_cost_and_usage(
+                    TimePeriod={
+                        "Start": str(start_date_input),
+                        "End": str(end_date_input),
+                    },
+                    # Granularity="MONTHLY",
+                    Granularity=granularity,
+                    Metrics=["UnblendedCost"],
+                    GroupBy=[  # group the result based on account ID
+                        {"Type": "DIMENSION", "Key": cost_groupby_key_input}
+                    ],
+                )
+                for item in account_cost_usage["ResultsByTime"]:
+                    time_period = item["TimePeriod"]
+                    for group in item["Groups"]:
+                        ID = group["Keys"][0]
+                        cost = group["Metrics"]["UnblendedCost"]["Amount"]
+                        results.append(
+                            {
+                                "TIME_PERIOD": time_period,
+                                "ID": ID,
+                                "COST": cost,
+                            }
+                        )
+
         # progress.update(task, advance=1)
         thread = threading.Thread(target=fetch_account)
         thread.start()
