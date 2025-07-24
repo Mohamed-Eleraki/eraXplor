8be3e920182c61e3fe83a0e1e30af47ea810cd76
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-07-24 12:08:07 +0300

    update version

--- src/eraXplor_aws/utils/__init__.py
+++ src/eraXplor_aws/utils/__init__.py
@@ -16,7 +16,7 @@ from .parser_utils import (
     parser_granularity_handler,
 )
 
-__version__ = "3.0.3"
+__version__ = "3.1.0"
 
 __all__=[
     'banner',
--- src/eraXplor_azure/utils/__init__.py
+++ src/eraXplor_azure/utils/__init__.py
@@ -8,7 +8,7 @@ from .parser_utils import parser
 from .cost_export_utils import cost_export
 
 
-__version__ = "3.0.3"
+__version__ = "3.1.0"
 
 __all__=[
     'banner',
